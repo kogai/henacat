f4a882960166513d07a8264877bcf064232580ec
小飼慎一 <kogai0121@gmail.com>
2017-04-02 17:52:17 +0900

    tmp

--- tests/main.rs
+++ tests/main.rs
@@ -8,10 +8,10 @@ use std::thread::spawn;
 #[test]
 fn e2e_should_get_response() {
     spawn(|| {
-        let mut server = server::Server::new("127.0.0.1:8000");
+        let mut server = server::Server::new("127.0.0.1:8888");
         server.listen();
     });
-    let client = client::Client::new("127.0.0.1:8000");
+    let client = client::Client::new("127.0.0.1:8888");
     let result = client.send();
 
     assert!(result.contains("It works!"));
